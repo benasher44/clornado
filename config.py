ac44786112e3b12f53f69338bada86f83cb1e169
Alex Haefner <alexhaefner@gmail.com>
2013-02-09 04:03:13 -0500

    Fixed some missing config flags

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
+import os.path
+
 debug = True
+tornado_settings = {
+    'cookie_secret': 'clornado', #TODO: Solve the issue of unique cookies and security
+    'gzip': True,
+    'login_url': '/login', #TODO: Investigate static_handler_class
+    'static_path': os.path.join(base_path, 'static'),
+    'template_path': os.path.join(base_path, 'templates'),
+    'ui_methods': include,
+    'ui_modules': uimodules,
+    'xsrf_cookies': True,
+
+}
 
 # Uncomment this to change your application port
 # application_port = 8080
